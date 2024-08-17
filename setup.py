1286c34b73b40c1a959ae4f752e3736a007b7e15
thiagosilva977 <thiagosilva977@hotmail.com>
2024-08-17 13:37:25 -0300

    Add setup py

---
+++ setup.py
@@ -0,0 +1,38 @@
+from pathlib import Path
+from setuptools import setup, find_packages
+
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text()
+
+REQUIRED_PKGS = [
+    'beautifulsoup4',
+    'click',
+    'lxml',
+    'numpy',
+    'pandas',
+    'pyarrow',
+    'requests',
+]
+EXTRAS_REQUIRE = {
+    "full": [
+        "torchvision",
+        'langchain'
+    ]}
+
+setup(
+    name="news-data-extractor",
+    version="0.0.1",
+    author="Thiago Silva",
+    author_email="",
+    description="Extract news in different sources.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/thiagosilva977/simple-news-search-engine",
+    packages=find_packages(),
+    classifiers=[],
+    python_requires=">=3.11",
+    install_requires=REQUIRED_PKGS,
+    extras_require=EXTRAS_REQUIRE,
+    include_package_data=True,
+    zip_safe=False
+)
